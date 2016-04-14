fbe37d696e21b54167719ab2246fb592c093da9f
Dylan Clement <dylanclement@gmail.com>
2016-04-14 21:05:52 +1000

    Add editorconfig file for go settings.

--- src/main.go
+++ src/main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"github.com/rwcarlsen/goexif/exif"
-//    "github.com/rwcarlsen/goexif/mknote"
-//    "github.com/xiam/exif"
-
-    "errors"
-    "fmt"
-    "log"
+	"errors"
+	"fmt"
+	"log"
 	"os"
-    "time"
+	"time"
 )
 
+// Helper
 func handleErr(err error) {
 	if err != nil {
-        fmt.Printf("Error:%s\n", err.Error())
+		fmt.Printf("Error:%s\n", err.Error())
 		os.Exit(1)
 	}
 }
@@ -27,35 +25,37 @@ func getDateTaken(fileName string) (time.Time, error) {
 	}
 
 	file, err := os.Open(fileName)
-	if err != nil { return time.Now(), err }
+	if err != nil {
+		return time.Now(), err
+	}
 
 	log.Printf("filename = %s\n", fileName)
 
-    date := time.Now()
+	date := time.Now()
 	data, err := exif.Decode(file)
-    if err != nil {
-        // file might noit have exif data, use os.Stat
-        stat, err := os.Stat(fileName)
-        handleErr(err)
+	if err != nil {
+		// file might not have exif data, use os.Stat
+		stat, err := os.Stat(fileName)
+		handleErr(err)
 
-        date = stat.ModTime()
-    } else {
-        date, _ = data.DateTime()
-    }
+		date = stat.ModTime()
+	} else {
+		date, _ = data.DateTime()
+	}
 
-    return date, err
-} 
+	return date, err
+}
 func main() {
 	args := os.Args[1:]
 
-    if len(args) < 1 {
-        fmt.Printf("Please pass a filename as a parameter")
-        os.Exit(1)
-    }
+	if len(args) < 1 {
+		fmt.Printf("Please pass a filename as a parameter")
+		os.Exit(1)
+	}
 
-    fileName := args[0]
+	fileName := args[0]
 
-    date, _ := getDateTaken(fileName)
+	date, _ := getDateTaken(fileName)
 
-    fmt.Printf("Date created = %s", date)
+	fmt.Printf("Date created = %s", date)
 }
