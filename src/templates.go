46db66f98cc6ded11fe4688cdc1b43e2d98c0bba
Dylan Clement <dylanclement@gmail.com>
2016-05-17 20:49:51 +1000

    Fix templates.

--- src/templates.go
+++ src/templates.go
@@ -6,6 +6,14 @@ const WebsiteTemplate = `<!doctype html>
 <head>
 	<title><%Title%></title>
 	<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' />
+	<style>
+		body { background-color: ghostwhite; }
+		h2 { padding-left: 30px; }
+		ul { list-style-type: none; }
+		.header { padding: 20px}
+		.img-thumbnail { height: 140px; }
+		.caption { padding-left: 45px; }
+	</style>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.js"></script>
 	<script type="text/javascript">
 		var myApp = angular.module('myApp',[]);
@@ -19,7 +27,6 @@ const WebsiteTemplate = `<!doctype html>
 
 			// gets thethumbnail name for the file
 			$scope.getThumbJpg = function(fileName) {
-				console.log("Test, " + fileName)
 				var idx = fileName.lastIndexOf(".");
 				return fileName.slice(0, idx) + "_thumb.jpg";
 			}
@@ -28,7 +35,10 @@ const WebsiteTemplate = `<!doctype html>
 </head>
 <body>
 	<div class="container" ng-controller="MainCtrl">
-		<a href="<%BACK%>"><%YEAR%></a><h2><%DATE%></h2>
+		<div class="header">
+			<a class="h2"href="<%BACK%>"><%YEAR%>/</a>
+			<span class="h2"><%DATE%></h2>
+		</div>
 		<div class="body">
 			<div ng-repeat="filename in files">
 				<div class="col-lg-3 col-md-4 col-xs-6 thumb">
@@ -44,8 +54,17 @@ const WebsiteTemplate = `<!doctype html>
 const FolderTemplate = `<!doctype html>
 <html lang="en" ng-app="myApp">
 <head>
-<title><%Title%></title>
-<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' />
+<title><%TITLE%></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<style>
+body { background-color: ghostwhite; }
+h2 { padding-left: 30px; }
+ul { list-style-type: none; }
+.img-thumbnail { height: 140px; }
+.caption { padding-left: 45px; }
+</style>
+
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.js"></script>
 <script type="text/javascript">
 	var myApp = angular.module('myApp',[]);
@@ -60,18 +79,16 @@ const FolderTemplate = `<!doctype html>
 </script>
 </head>
 <body>
-	<a href="<%Back%>">Back</a>
 	<div class="container" ng-controller="MainCtrl">
-		<h1><%Title%></h1>
+		<h2><%TITLE%></h1>
 		</br>
-		<div class="navbar" />
 		<div class="body">
-			<div ng-repeat="date in dates">
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-					<p>{{date.date}}</p>
+			<ul id="image-list" class="row">
+				<li ng-repeat="date in dates" class="col-lg-2 col-md-4 col-sm-6">
 					<a href="{{date.date}}/index.html"><img ng-src="{{date.thumb}}" class="img-thumbnail" /></a>
-				</div>
-			</div>
+					<span class="caption">{{date.date}}</span>
+				</li>
+			</ul>
 		</div>
 	</div>
 </div>
